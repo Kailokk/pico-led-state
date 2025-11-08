a93092704770bb6343df415937bd4597b7e7d5a3
Kailokk <kailokk.converse@gmail.com>
2025-11-08 00:07:15 +0000

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        nativeBuildInputs = with pkgs; [ ];
+
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rustfmt"
+            "rustc-dev"
+          ];
+          targets = [ "thumbv8m.main-none-eabihf" ];
+        };
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            nixfmt
+            curl
+            docker
+            git
+
+            toolchain
+            cargo-expand
+            picotool
+            openssl
+
+            sccache
+          ];
+
+          RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache"
+        };
+      }
+    );
+}
